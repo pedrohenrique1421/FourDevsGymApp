bc70223354aefbe574e08e9891e52fc35694e10d
wilkenio <wilkeniopereiraguitarra@gmail.com>
2024-07-21 07:39:52 -0300

    feat: inicio do layout de ficha de treino

--- src/Telas/Principais/Treinos_p/index.js
+++ src/Telas/Principais/Treinos_p/index.js
@@ -1,4 +1,4 @@
-import { Text, SafeAreaView, View, StatusBar, Dimensions } from "react-native";
+import { Text, SafeAreaView, View, StatusBar, Dimensions, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./style";
 import Global_Colors from "../../../Scripts/GLobal/Global_Colors";
@@ -13,9 +13,28 @@ export default function Treino_p({ chave }) {
             {/* NavBar */}
             <NavBar_c page={"Treinos_p"} />
             <View style={[styles.cpContainer, { backgroundColor: Global_Colors.BW_PRIMARY_COLOR }]}>
-                <Text style={styles.cpTitle}>Pagina de Treinos</Text>
-                <Text style={[styles.cpSubTitle, { color: Global_Colors.BW_SECONDARY_COLOR }]}>Onde terá treinos</Text>
+
+                {/* <Text style={[styles.cpSubTitle, { color: Global_Colors.BW_SECONDARY_COLOR }]}>Onde terá treinos</Text> */}
+
+                <View style={styles.treino}>
+                <Text style={styles.nomeTreino}>Treino 1</Text>
+                    <View style={styles.dia}>
+                        <Text style={styles.nomeDia}>Dia 1</Text>
+                        <View style={styles.exercicio}>
+                        <Image
+                source={{ uri: 'https://www.hipertrofia.org/blog/wp-content/uploads/2023/09/smith-bench-press.gif' }}
+                style={styles.imageExercicio}
+            />
+                            <Text style={styles.nomeExercicio}>Supino Retoo</Text>
+                            <Text style={styles.series}>3</Text>
+                            <Text>X</Text>
+                            <Text style={styles.rep}>12</Text>
+                        </View>
+                    </View>
+                </View>
             </View>
+
+
         </SafeAreaView>
     );
 }
--- src/Telas/Principais/Treinos_p/style.js
+++ src/Telas/Principais/Treinos_p/style.js
@@ -9,7 +9,6 @@ const styles = StyleSheet.create({
     // Conteudo da pagina
     cpContainer: {
         width: "100%",
-        height: "100%",
         justifyContent: "center",
         alignItems: "center",
         position: "absolute",
@@ -19,12 +18,51 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "700",
         color: Global_Colors.PRIMARY_COLOR,
+        marginTop:90
     },
     cpSubTitle: {
         fontSize: 18,
         fontWeight: "600",
         fontStyle: "italic",
     },
+    nomeDia:{
+        backgroundColor:Global_Colors.PRIMARY_COLOR,
+        color:Global_Colors.BW_PRIMARY_COLOR,
+        textAlign:"center",
+        fontWeight:"600",
+        borderTopLeftRadius:5,
+        borderTopRightRadius: 5,
+        padding:10
+    },
+    treino:{
+        width:"100%",
+        alignItems:"center"
+    },
+    nomeTreino:{
+        marginTop:100,
+        marginBottom:20,
+        fontSize:20,
+        color:Global_Colors.PRIMARY_COLOR,
+        fontWeight:"900"
+    },
+    dia:{
+        width:"85%"
+    },
+    exercicio:{
+        padding:5,
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"space-between",
+        borderBottomWidth:1,
+        borderColor:Global_Colors.QUARTENARY_COLOR,
+    },
+    imageExercicio:{
+        width:100,
+        height:70
+    },
+    rep:{
+        marginRight:5
+    }
 });
 
 export default styles;
